90daa09354eb6e9e85ba8af08ca6963c11a90ace
Michel Erokhov <mixaileroxov@gmail.com>
2023-02-14 16:56:30 -0500

    fix test

--- test/whitelistCircuit.ts
+++ test/whitelistCircuit.ts
@@ -1,10 +1,10 @@
 import { wasm as wasmTester } from 'circom_tester'
-import getEdDSAInputs from '../utils/inputs/getAllowMapInputs'
+import getAllowMapInputs from '../utils/inputs/getAllowMapInputs'
 
-describe('EdDSAChecker circuit', function () {
+describe('AllowMapChecker circuit', function () {
   before(async function () {
-    this.circuit = await wasmTester('circuits/EdDSAChecker.circom')
-    this.baseInputs = await getEdDSAInputs()
+    this.circuit = await wasmTester('circuits/AllowMapChecker.circom')
+    this.baseInputs = await getAllowMapInputs()
   })
   it('should generate the witness successfully', async function () {
     const witness = await this.circuit.calculateWitness(this.baseInputs)
